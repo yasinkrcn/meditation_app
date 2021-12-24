0f9eb247b7587a40b0c95df6e0567f6553f3e8d0
yasokraker <23yasin1999@gmail.com>
2021-12-24 16:43:13 +0300

    color eklendi

---
+++ lib/feature/homepage/view/widget/home_page_app_bar.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class HomePageAppBar extends StatelessWidget implements PreferredSizeWidget {
+  const HomePageAppBar({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+
+  @override
+  Size get preferredSize => const Size.fromHeight(50);
+}
---
+++ lib/feature/homepage/view/widget/home_page_body.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:meditation_app/core/constants/theme/app_colors.dart';
+
+class HomePageBody extends StatelessWidget {
+  const HomePageBody({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: AppColors.blackColor,
+    );
+  }
+}
